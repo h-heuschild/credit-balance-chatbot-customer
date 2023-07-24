95e69ff13b5ba0d08dc77142f8666e2fd8568461
Hanes Heuschild <hanes_heuschild@web.de>
2023-07-24 16:05:29 +0200

    Commandline Tool erstellt um Creditdaten zu einem Kunden abzurufen cd /Users/hanesheuschild/Documents/Lernen/Programmieren/Java/chatbot-viind-challenge/credit-balance-chatbot-customer ; /usr/bin/env /Library/Java/JavaVirtualMachines/jdk-20.jdk/Contents/Home/bin/java @/var/folders/_f/t4d4xs1n32n6s9rsgqq1nkxw0000gn/T/cp_dl75u3wf1ib19ff5enyrln1h.argfile de.viind.App

--- customer-credit-balance/src/main/java/de/viind/App.java
+++ customer-credit-balance/src/main/java/de/viind/App.java
@@ -8,22 +8,72 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import org.apache.http.client.utils.URIBuilder;
+
 public class App {
+
+    private static String AUTH_HEADER = "Authorization";
+    private static String BEARER_TOKEN = "Bearer pLdY3ulLws0XboEe5Z7BNyyYWSNrGt3a";
+
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println(
+                    "Bitte geben sie einen der beiden Modus ein: 'Creditabfrage' oder 'Crediterhoehung' mit der passenden Kundennummer dazu.");
+        } else if (args[0].equals("Creditabfrage")) {
+            processCreditabfrage(Integer.parseInt(args[1]));
+        } else if (args[0].equals("Crediterhoehung")) {
+            processCrediterhoehung(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        } else {
+            System.out.println(
+                    "Bitte geben sie einen der beiden Modus ein: 'Creditabfrage' oder 'Crediterhoehung' mit der passenden Kundennummer dazu.");
+        }
+    }
+
+    private static void processCrediterhoehung(Integer customerId, Integer valueToRaiseCredit) {
+    }
+
+    private static void processCreditabfrage(Integer customerId) {
         HttpClient httpClient = HttpClient.newHttpClient();
+
         try {
+            URI uri = new URIBuilder("https://sdp-sandbox-billing.cluster01.viind.io/graphql")
+                    .addParameter("query", "{billing(customerId:\"1234\"){monthlyCredits remainingCredits}}")
+                    .build();
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("https://sdp-sandbox-billing.cluster01.viind.io/graphql/"))
-                    .header("key1", "value1")
-                    .header("key2", "value2")
+                    .uri(uri)
+                    .header(AUTH_HEADER, BEARER_TOKEN)
                     .GET()
                     .build();
 
             HttpResponse<String> httpResponse = httpClient.send(request, BodyHandlers.ofString());
-            System.out.println(httpResponse.body());
+            String ausgabeText = String.format(
+                    "Der Kunde %d hat noch %d verfuegbare Credits von insgesamt %d monatlichen Credits",
+                    customerId,
+                    getRemainingBillingCredits(httpResponse.body()),
+                    getMonthlyBillingCredits(httpResponse.body()));
+            System.out.println(ausgabeText);
         } catch (URISyntaxException | IOException | InterruptedException e) {
             System.out.println("Es ist ein Problem aufgetreten: " + e.getMessage());
         }
+    }
+
+    private static int getMonthlyBillingCredits(String queryString) {
+        String variableString = "monthlyCredits";
+        int startIndexOfMonthlyCredits = queryString.indexOf(variableString) + variableString.length() + 2;
+        int endIndexOfMonthlyCredits = queryString.indexOf(",");
+        int monthlyBillingCredits = Integer
+                .parseInt(queryString.substring(startIndexOfMonthlyCredits, endIndexOfMonthlyCredits));
+        return monthlyBillingCredits;
+    }
+
+    private static int getRemainingBillingCredits(String queryString) {
+        String variableString = "remainingCredits";
+        int startIndexOfMonthlyCredits = queryString.indexOf(variableString) + variableString.length() + 2;
+        int endIndexOfMonthlyCredits = queryString.indexOf("}");
+        int monthlyBillingCredits = Integer
+                .parseInt(queryString.substring(startIndexOfMonthlyCredits, endIndexOfMonthlyCredits));
+        return monthlyBillingCredits;
     }
 }
